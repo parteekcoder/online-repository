419bec3889a7e8b7d777417e4b1e13a74ae01528
MadhurAggarwal <madhuraggarwalofficial@gmail.com>
2022-11-06 16:44:03 +0530

    fourth commit

--- online-resorce/frontend/src/components/Navbar.js
+++ online-resorce/frontend/src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar=()=>{
       
     </ul>
     <form className="form-inline my-2 my-lg-0">
-      {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
+      {<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" /> }
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
