149dfc0b38652f5fff43b4cf9010a389fc282bfe
parteekcoder <parteekgoyal118@gmail.com>
2022-11-06 14:34:56 +0530

    5

--- online-resorce/backend/db.js
+++ online-resorce/backend/db.js
@@ -1,19 +1,35 @@
-const OracleDB = require('oracledb')
-const oracle=require('oracledb')
+// const OracleDB = require('oracledb')
 
- OracleDB.outFormat=OracleDB.OUT_FORMAT_OBJECT;
-let conn;
- const connectDB=async()=>{
-    try {
-        conn=await OracleDB.getConnection({
-            user:"parteek",
-            password:"Parteek2803",
-            connectString:"localhost/XEPDB1",
-            conn
-        });
-        console.log('connected');
-    } catch (error) {
-        console.log(error);
-    }
- }
- connectDB();
+//  OracleDB.outFormat=OracleDB.OUT_FORMAT_OBJECT;
+// let conn;
+//  const connectDB=async()=>{
+//     try {
+//         conn=await OracleDB.getConnection({
+//             user:"parteek",
+//             password:"Parteek2803",
+//             connectString:"localhost/XEPDB1",
+            
+//         });
+//         console.log('connected');
+//     } catch (error) {
+//         console.log(error);
+//     }
+//  }
+//  connectDB();
+// const sqlite3 = require('sqlite3').verbose();
+
+// // open database in memory
+// let db = new sqlite3.Database(':memory:', (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Connected to the in-memory SQlite database.');
+// });
+
+// // close the database connection
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+// });
